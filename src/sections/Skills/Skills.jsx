2bdeb865e3f2d8163994e168838815797ce8c4f1
jomorales9375 <107601827+jomorales9375@users.noreply.github.com>
2024-09-14 14:48:01 -0700

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,18 @@
 import styles from './SkillStyles.module.css'
-import checkMarkIcon from '../../assets/checkmark-dark.svg'
 import SkillList from '../../common/SkillList'
+import { useTheme } from '../../common/ThemeContext';
+import checkMarkDarkIcon from '../../assets/checkmark-dark.svg';
+import checkMarkLightIcon from '../../assets/checkmark-light.svg'
+
 
 function Skills() {
+
+   const { theme } = useTheme(); 
+
+ 
+   const checkMarkIcon = theme === 'light' ? checkMarkLightIcon : checkMarkDarkIcon;
+   
+
   return (
     <section id="skills" className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
@@ -20,7 +30,7 @@ function Skills() {
         <div className={styles.skillList}>
            <SkillList src={checkMarkIcon} skill="React" />
            <SkillList src={checkMarkIcon} skill="Angular" />
-           <SkillList src={checkMarkIcon} skill="Vue" />
+           <SkillList src={checkMarkIcon} skill="Bootstrap" />
            <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
         
 
@@ -29,9 +39,9 @@ function Skills() {
         <hr />
         <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="SQL" />
+           <SkillList src={checkMarkIcon} skill="MongoDB" />
            <SkillList src={checkMarkIcon} skill="Git" />
-           <SkillList src={checkMarkIcon} skill="Bootstrap" />
-           <SkillList src={checkMarkIcon} skill="Webpack" />
+           <SkillList src={checkMarkIcon} skill="REST API" />
            </div>
 
 
